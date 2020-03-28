3b2035975d90a3af754ee78e6f3fc18c5dbe1042
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-28 18:07:53 -0500

    [Feature] Created routes users, posts, profile and auth, also connected with server

--- server.js
+++ server.js
@@ -4,9 +4,16 @@ const connectDB = require('./config/db');
 
 const app = express();
 
+// Database connection
 connectDB();
 
 app.get('/', (req, res) => res.send('API running'));
+
+// Routes definition
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
