63220ecda7d9a5083a3977edb7ceb32e9d2fe2f2
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-28 17:51:02 -0500

    [Feature] Configured connection to mongo with config package

---
+++ config/db.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    console.log("MongoDB Connected...")
+  } catch(e) {
+    console.log(e.message);
+
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
