4adce061a6c698a0ac7cee02fd1763e24541b32d
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-28 19:37:12 -0500

    [Feature] Created middleware to authorize authentication user and configured auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,5 +1,8 @@
 const express = require('express');
 
+const auth = require('../../middlewares/auth');
+const User = require('../../models/User');
+
 const router = express.Router();
 
 /**
@@ -7,6 +10,14 @@ const router = express.Router();
  * @desc    Test Route
  * @access  Public
  */
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch(err) {
+    console.error(err.message);
+    res.status(400).send('Server Error')
+  }
+});
 
 module.exports = router;
