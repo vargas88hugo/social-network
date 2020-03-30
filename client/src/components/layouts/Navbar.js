794389555e983927099a1334f142e931cd4ae10f
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-29 19:49:52 -0500

    [Feature] Created react project and configured styles, main components and routes

---
+++ client/src/components/layouts/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faNetworkWired } from '@fortawesome/free-solid-svg-icons';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar bg-dark">
+      <h1>
+        <Link to="/"><FontAwesomeIcon icon={faNetworkWired} /> Social Network</Link>
+      </h1>
+      <ul>
+        <li><Link to="#">Developers</Link></li>
+        <li><Link to="/register">Register</Link></li>
+        <li><Link to="/login">Login</Link></li>
+      </ul>
+    </nav>
+  );
+}
+ 
+export default Navbar;
