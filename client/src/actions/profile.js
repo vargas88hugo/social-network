0a3fb42a22e15ae217a744c11b30040813063df4
Hugo Vargas <vargas88hugo@gmail.com>
2020-03-31 18:05:49 -0500

    [Update] Connected profile creator with backend and configured alerts

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,6 +6,9 @@ import {
   PROFILE_ERROR
 } from './types';
 
+/**
+ * Function action that get the current progile with all the information
+ */
 export const getCurrentProfile = () => async dispatch => {
   try {
     const res = await axios.get('/api/profile/me');
@@ -24,3 +27,37 @@ export const getCurrentProfile = () => async dispatch => {
     });
   }
 }
+
+export const createProfile = (formData, history, edit = false) => async dispatch => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data
+    });
+
+    dispatch(setAlert(edit ? 'Profile Updated' : 'Profile Created', 'success'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (err) {
+    const errors = err.response.data.errors;
+
+    if (errors) {
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')));
+    }
+
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status }
+    });
+  }
+};
--- client/src/components/profile-forms/CreateProfile.js
+++ client/src/components/profile-forms/CreateProfile.js
@@ -1,11 +1,13 @@
 import React, { useState, Fragment } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
 
+import { createProfile } from '../../actions/profile'; 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faFacebook, faYoutube, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 
-const CreateProfile = props => {
+const CreateProfile = ({ createProfile, history }) => {
   const [formData, setFormData] = useState({
     company: '',
     website: '',
@@ -43,6 +45,11 @@ const CreateProfile = props => {
     [e.target.name]: e.target.value
   });
 
+  const onSubmit = e => {
+    e.preventDefault();
+    createProfile(formData, history);
+  }
+
   return (
     <Fragment>
       <h1 className="large text-primary">
@@ -53,7 +60,7 @@ const CreateProfile = props => {
         profile stand out
       </p>
       <small>* = required field</small>
-      <form className="form">
+      <form className="form" onSubmit={e => onSubmit(e)}>
         <div className="form-group">
           <select 
             name="status" 
@@ -248,7 +255,7 @@ const CreateProfile = props => {
 }
 
 CreateProfile.propTypes = {
+  createProfile: PropTypes.func.isRequired
+};
 
-}
-
-export default CreateProfile
+export default connect(null, { createProfile })(withRouter(CreateProfile));
