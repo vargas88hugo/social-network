aae2bca53b9253c1962d2f206b7ba3701fa8991a
Hugo Vargas <vargas88hugo@gmail.com>
2020-04-03 13:36:30 -0500

    [Feature] Removed unnecessary dependencies and prepared for deployment

--- server.js
+++ server.js
@@ -10,13 +10,19 @@ connectDB();
 // Init middlewares
 app.use(express.json({ extended: false })); 
 
-app.get('/', (req, res) => res.send('API running'));
-
 // Routes definition
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
